caa7ad0aef926ea9a6ab016fb2599b98d67ca560
Roman Elizarov <elizarov@gmail.com>
2015-02-16 22:15:14 +0300

    Additional method to track press time, no longer uses Timeout lib

--- Button/Button.cpp
+++ Button/Button.cpp
@@ -1,27 +1,57 @@
 #include "Button.h"
 
-const int DEBOUNCE = 50; // 50 ms
+const int DEBOUNCE_INTERVAL = 50; // 50 ms
 
 Button::Button(uint8_t pin) :
-  _pin(pin) 
+  _pin(pin),
+  _state(UP) 
 {
   pinMode(pin, INPUT); // just in case
   digitalWrite(pin, 1); // pullup
 }
 
 bool Button::check() {
-  bool state = digitalRead(_pin) == 0;
-  if (state == _state) {
-    _timeout.disable();
-    return false;
+  if (digitalRead(_pin) == 0) {
+    // button is down
+    switch (_state) {
+    case UP:
+      _time = millis(); 
+      _state = DEBOUNCE;
+      break;
+    case DEBOUNCE:
+      if (long(millis() - _time) >= DEBOUNCE_INTERVAL) {
+        _state = DOWN;
+        return true; // pressed state changed
+      }
+    case DOWN: 
+      break; // nothing to do
+    }
+  } else {
+    // button is up
+    switch (_state) {
+    case UP:
+      break; // nothing to do
+    case DEBOUNCE:
+      _time = 0; // was down for too short, forget
+      _state = UP;   
+      break;
+    case DOWN:
+      _state = UP; 
+      _time = millis() - _time; // remember how long it was pressed
+      return true; // pressed state changed
+    }
   }
-  if (!_timeout.enabled()) {
-    _timeout.reset(DEBOUNCE);
-    return false;
-  }
-  if (!_timeout.check())
-    return false;
-  _state = state;
-  return true;
+  return false; // no state change
 }
 
+long Button::pressed() {
+  return _state == DOWN ? long(millis() - _time) : 0;
+}
+
+long Button::released() {
+  if (_state != UP)
+    return 0; // not released
+  long duration = _time;
+  _time = 0; // report release time just once
+  return duration;
+}
--- Button/Button.h
+++ Button/Button.h
@@ -2,23 +2,68 @@
   Simple debounced button driver.
   Button is connected between a specified pin and a ground. 
   Internull pullup on the pin is used.
+
+  Basic usage:
+
+    Button button(pin);
+
+  Must periodically call for debouncing logic:
+
+    button.check();
+
+  Can use result of the check call to determine when state changes.
+
+  For a simple code that depends on up / down state of a button, do:
+
+    if (button) {
+      // a code that is invoked when button is pressed down
+    }
+
+  To detect long-press of a button while the button is still pressed,
+  use the following code:
+
+    if (button.pressed() >= THRESHOLD) {
+      // do something when button was pressed longer that THRESHOLD
+    }
+
+  To detect button release and make decision based on its press time,
+  use the following code (can call release only once, thus use a variable):
+
+    long time = button.released();
+    if (time >= THRESHOLD) {
+      // that was long press
+    } else if (time >= 0) {
+      // was released in time below THRESHOLD
+    }
+
+  Author: Roman Elizarov
 */
+
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
 #include <Arduino.h>
-#include <Timeout.h>
 
 class Button {
-private:
-  uint8_t _pin;
-  bool _state;
-  Timeout _timeout;
-
 public:
   Button(uint8_t pin);
-  bool check();    // returns true on any change, must be called periodically
-  operator bool() { return _state; } // return true when pressed down
+  bool check();        // returns true on any change, must be called periodically
+  operator bool();     // returns true when in pressed state
+  long pressed();      // returns number >= 0 when pressed down indicating how long it is being pressed
+  long released();     // returns number >= 0 only once on pressed / released transition indicating how long it was pressed
+
+private:
+  enum State { UP, DEBOUNCE, DOWN };
+
+  uint8_t _pin;
+  State _state;
+  long _time;
 };
+
+// ------------ short method implementations are inline here ------------
+
+inline Button::operator bool() {
+  return _state == DOWN;
+}
 
 #endif
