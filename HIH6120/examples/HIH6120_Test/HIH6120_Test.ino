ae59e5761609d2208f37d4759f2278dd759883ac
Roman Elizarov <elizarov@gmail.com>
2015-02-18 00:43:54 +0300

    Includes water wapor pressure

--- HIH6120/examples/HIH6120_Test/HIH6120_Test.ino
+++ HIH6120/examples/HIH6120_Test/HIH6120_Test.ino
@@ -14,12 +14,14 @@
   4 - SDA --> A4
 
   This sketch prints measurements to serial and blinks led on each one.
+  It also computes water vapor pressure.
 */
 
 #include <Timeout.h>
 #include <HIH6120.h>
 #include <TWIMaster.h>
 #include <FixNum.h>
+#include <Humidity.h>
 
 const uint8_t LED_PIN = 13;
 
@@ -30,14 +32,18 @@ Timeout ledTimeout;
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(57600);
-  Serial.println("=== HIH6120 Test ===");
+  Serial.println(F("=== HIH6120 Test ==="));
 }
 
 void loop() {
   if (hih6120.check()) {
-    Serial.println("--- HIH6120 Test ---");
-    Serial.print("T="); Serial.println(hih6120.getTemp().format());
-    Serial.print("H="); Serial.println(hih6120.getRH().format());
+    Serial.println(F("--- HIH6120 Test ---"));
+    HIH6120::temp_t temp = hih6120.getTemp();
+    HIH6120::rh_t rh = hih6120.getRH();
+    wvp_t wvp = waterVaporPressure(temp, rh);
+    Serial.print(F("T=")); Serial.print(temp.format()); Serial.println(F(" Celcius"));
+    Serial.print(F("H=")); Serial.print(rh.format());   Serial.println(F(" %"));
+    Serial.print(F("P=")); Serial.print(wvp.format());  Serial.println(F(" mbar"));
     digitalWrite(LED_PIN, 1);
     ledTimeout.reset(hih6120.getTemp() ? 1000 : 250);
   }
@@ -47,7 +53,7 @@ void loop() {
   // Print state periodically if there is some error or measurement in progress
   if (stateTimeout.check()) {
     if (hih6120.getState() != 0) {
-      Serial.print("HIH6120 State = "); Serial.println(hih6120.getState(), HEX);
+      Serial.print(F("HIH6120 State = ")); Serial.println(hih6120.getState(), HEX);
     }
     stateTimeout.reset(1000);
   }
